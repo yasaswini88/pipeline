04695d13b50d867e4b9f7bb289b6d48af706d32b
yasaswini88 <137493400+yasaswini88@users.noreply.github.com>
2024-12-16 19:30:05 -0500

    Initial commit for Spring Boot project

---
+++ src/main/java/com/example/pipeline/PipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineApplication.class, args);
+	}
+
+}
